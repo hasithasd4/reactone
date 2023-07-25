1f2cea954aee53b389c97b8c382740e62f0a0f0c
hasithasd4 <hasithasd4@gmail.com>
2023-07-25 21:16:55 +0530

    add sahan

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Sahan from './component/Sahan';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>sahan hasitha</h1>
+      <Sahan/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Sahan.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Sahan() {
+  return (
+    <div>Sahan</div>
+  )
+}
